f17fa994e956abc65bec7d58fe0c19018ee64ffa
Robert H. <horvath.robo@proton.me>
2026-02-24 11:50:53 +0100

    Enable DynamoDB Stream with NEW_IMAGE view type for SES trigger

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,9 @@
 resource "aws_dynamodb_table" "dynamodb" {
-  name         = "adventureconnect-submissions"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "submissionId"
+  name             = "adventureconnect-submissions"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+  hash_key         = "submissionId"
 
   attribute {
     name = "submissionId"
