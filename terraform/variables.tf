50c50ed078178c5affcffe2ef33e217fd5924ea3
Robert H. <horvath.robo@proton.me>
2026-02-24 11:54:35 +0100

    Add Terraform variables for SES sender and recipient email addresses

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "sender_email" {
+  description = "Verified SES sender email address"
+  type        = string
+}
+
+variable "recipient_email" {
+  description = "Verified SES recipient email address for contact form notifications"
+  type        = string
+}
