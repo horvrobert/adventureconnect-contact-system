d9370b73e9c49d35c49ac8e711f4e1b1dc1415cc
Robert H. <horvath.robo@proton.me>
2026-02-25 13:19:54 +0100

    Add s3_bucket_name variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "recipient_email" {
   description = "Verified SES recipient email address for contact form notifications"
   type        = string
 }
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for contact form submissions"
+  type        = string
+}
